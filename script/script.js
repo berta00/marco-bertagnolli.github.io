08603e1b4199af0fc47e0acb01f80efd6627db6c
s3rgi0s <bertagnollimarco999@gmail.com>
2022-10-08 21:49:45 +0200

    homepage almost finished

--- script/script.js
+++ script/script.js
@@ -1,3 +1,35 @@
+function delay(ms){
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+function textWrite(element, words, it1, it2){
+    document.querySelector(element).innerHTML += words[it1][it2];
+}
 
+function textDelete(element, words, time, it1, itDel){
+    document.querySelector(element).innerHTML = words[it1].slice(0, itDel);
+}
 
+async function textChangeAnimation(element, time1, time2, words){
+    while(true){
+        for(let a = 0; a < words.length; a++){
+            document.querySelector(element).innerHTML = " ";
+            
+            for(let b = 0; b < words[a].length; b++){
+                await delay(time1);
+                await textWrite(element, words, a, b);
+            }
+            
+            await delay(time2);
+            
+            for(let c = words[a].length; c > 0; c--){
+                let tempC = c - 1;
+                await delay(time1);
+                await textDelete(element, words, time1, a, tempC);
+            }
+            await delay(150);
+        }
+    }
+}
+
+textChangeAnimation(".text", 120, 1800, ["Developer", "Student", "Gamer", "Designer"]);
